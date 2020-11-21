a9a36fefed320416e4d46f6b47310edc0a26ab91
Tania <tanialasell@gmail.com>
2020-11-21 17:07:12 -0500

    add froggie fun

---
+++ Calculator - Storyline output/story_content/user.js
@@ -0,0 +1,21 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6a7BR1KB8lW":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var star = star_storyline;
+document.getElementById("star_storyline").addEventListener("click", myFunction);
+
+function myFunction() {
+  alert ("Hello World!");
+}
+player.GetVar();
+}
+
