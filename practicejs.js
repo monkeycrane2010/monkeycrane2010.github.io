6bc6e7cf77a0b0670d4d5c06051c4bf444fb1b13
Tania <tanialasell@gmail.com>
2020-12-25 16:12:08 -0500

    drag from the YOUTUBE video

--- practicejs.js
+++ practicejs.js
@@ -1,101 +1,41 @@
 let whiteboard = document.getElementById('whiteboard');
 
-
-
-let firstparagraph = document.getElementById('plaintext');
-let xpos = document.getElementById('cursorx');
-let ypos = document.getElementById('cursory');
-
-let dragged;
 let monkey = document.getElementById('monkey');
 let sheep = document.getElementById('sheep');
 let banana = document.getElementById('banana');
 
 
 
+let firstparagraph = document.getElementById('plaintext');
+let xpos = document.getElementById('cursorx');
+let ypos = document.getElementById('cursory');
 
-function dragStart(event){
- 
- let target = event.target;
- console.log(target)
-  if(target && target.nodeName === 'IMG') {
-   dragged = target;
-   event.dataTransfer.setData('text', event.target.id);
-   event.dataTransfer.dropEffect = 'move';
-  }
-}
+let el = document.querySelector('.item');
+el.addEventListener('mousedown', mouseDown);
 
-monkey.addEventListener('dragstart', dragStart, false);
-sheep.addEventListener('dragstart', dragStart, false);
-banana.addEventListener('dragstart', dragStart, false);
+function mouseDown(e){
+    window.addEventListener('mousemove', mouseMove);
+    window.addEventListener('mouseup', mouseUp);
 
+    let prevX = e.clientX;
+    let prevY = e.clientY;
 
-///
-///  DROP TARGET
-//
-function dragOver(event){
-    event.preventDefault();
+    function mouseMove(e){
+       let newX = prevX - e.clientX;
+       let newY = prevY - e.clientY;
 
-}
+       let rect = el.getBoundingClientRect();
 
-function dragEnter(event){
-    //whiteboard.style.backgroundColor = 'orange'
-    firstparagraph.style.color = 'green'
-    firstparagraph.innerText = 'Drag Enter';
-    xpos.innerText = 'Client x = ' + event.clientX;
-    ypos.innerText = 'Client y = ' + event.clientY;
+       el.style.left = rect.left - newX + "px";
+       el.style.top = rect.top - newY + "px";
 
-    const target = event.target;
-    if(target){
-        event.preventDefault();
-        event.dataTransfer.dropEffect = 'move';
-        whiteboard.style.background = '#1f904e';
-        console.log(event)
-    }
-  
- }
+       prevX = e.clientX;
+       prevY = e.clientY;
 
-
- function onDrop(event){
-    const target = event.target;
-    if(target){
-        whiteboard.style.backgroundColor = '';
-        event.preventDefault();
-        dragged.parentNode.removeChild(dragged);
-        dragged.style.opacity = '';
-        target.appendChild(dragged);
-        
     }
 
+    function mouseUp(){
+       window.removeEventListener('mousemove', mouseMove);
+       window.removeEventListener('mouseup', mouseUp);
+    }
 }
-
-function mouseMove(event){
-    firstparagraph.style.color = 'purple'
-    firstparagraph.innerText = 'Mouse Move';
-    xpos.innerText = event.x;
-    ypos.innerText = event.y;
-
-}
-
-function mouseUp(event){
-    firstparagraph.style.color = 'pink'
-    firstparagraph.innerText = 'Mouse UP';
-    xpos.innerText = event.x;
-    ypos.innerText = event.y;
-
-}
-
-whiteboard.addEventListener('dragover', dragOver, false);
-whiteboard.addEventListener('dragenter', dragEnter, false);
-whiteboard.addEventListener('drop', onDrop, false);
-
-whiteboard.addEventListener('mousemove', mouseMove, false);
-whiteboard.addEventListener('mouseup', mouseUp, false);
-
-let monkey2 = document.createElement('img');
-monkey2.setAttribute("src", "images/monkey.png");
-whiteboard.appendChild(monkey2);
-
-let sheep2 = document.createElement('img');
-sheep2.setAttribute("src", "images/sheep.png");
-whiteboard.appendChild(sheep2);
