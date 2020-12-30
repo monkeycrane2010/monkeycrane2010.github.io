41f96e6837d00d781229cf1a8fd5a516581a18a1
Tania <tanialasell@gmail.com>
2020-12-30 00:33:49 -0500

    drag and drop WORKS

--- draggable.js
+++ draggable.js
@@ -1,116 +1,75 @@
-let image_placeholder1 = document.getElementById('img_placeholder1');
-let image_placeholder2 = document.getElementById('img_placeholder2');
 let imageplaceholder = [];
 
 let monkey = document.getElementById('monkey');
 let sheep = document.getElementById('sheep');
 let banana = document.getElementById('banana');
+let dropzoneOne = document.getElementById('dropzone1');
+let dropzoneTwo = document.getElementById('dropzone2');
 
-let monkeybtn = document.getElementById('monkeybtn');
-let leftbtn = document.getElementById('leftbtn');
-let rightbtn = document.getElementById('rightbtn');
 
 ///////
-////   RUN PROGRAM
+////   DRAG 
 ///////
-sheep.addEventListener('mousedown', mouseDown);
 
-function mouseDown(e){
-    window.addEventListener('mousemove', mouseMove);
-    window.addEventListener('mouseup', mouseUp);
-
-    let prevX = e.clientX;
-    let prevY = e.clientY;
-
-    function mouseMove(e){
-       let newX = prevX - e.clientX;
-       let newY = prevY - e.clientY;
-
-       let rect = sheep.getBoundingClientRect();
-
-       sheep.style.left = rect.left - newX + "px";
-       sheep.style.top = rect.top - newY + "px";
-
-       prevX = e.clientX;
-       prevY = e.clientY;
-
-    }
-
-    function mouseUp(){
-       window.removeEventListener('mousemove', mouseMove);
-       window.removeEventListener('mouseup', mouseUp);
-    }
-}
-
-/////////////
-//////
-/////////////
-
-
-class Imagefactory {
-   constructor(url, id) {
-    this.url = url;
-    this.id = id;
-   }
-
-   attribute(){
-     console.log(this.url);
-     console.log(this.id);
-     console.log('this is ATTRIBUTE');
-    
-   }
-
-   add(){
-      
-      for(let x = 0; x <= imageplaceholder.length; x++){
-         let imgNew = document.createElement('img');
-         imgNew.setAttribute('src', this.url);
-         imgNew.setAttribute('id', this.id + "_" + imageplaceholder.length);
-         image_placeholder1.appendChild(imgNew);
-         imageplaceholder.push(imgNew);
-         return imageplaceholder; // REQUIRED for inventory
-   }
+function monkeyActions(event) {
+  console.log(event.target.id);
+  event.dataTransfer.setData("text", event.target.id);
   
+}
+
+
+function sheepActions(event) {
+   console.log(event.target.id);
+   event.dataTransfer.setData("text", event.target.id);
+   
+ }
+
+
+ function bananaActions(event) {
+   console.log(event.target.id);
+   event.dataTransfer.setData("text", event.target.id);
+   
+ }
+
+
+///////
+////   DROP ZONE
+///////
+function allowDrop(event) {
+   event.preventDefault();
+}
+
+
+
+function dropzone1(event) {
+   
+   event.preventDefault();
+
+   if (event.type === "drop"){
+      let data = event.dataTransfer.getData("text");
+      event.target.appendChild(document.getElementById(data));
    }
 
 }
 
-let monkey2 = new Imagefactory("images/monkey.png", monkey.id);
-let banana2 = new Imagefactory("images/banana.png", banana.id);
 
-monkeybtn.addEventListener("click", function() {
-   if(image_placeholder1.hasChildNodes()){
-       monkey2.attribute();
-     } else {
-       monkey2.add();
-     }
- }, false); // NEED HELP PLEASE - How do I make 'monkey2.add' here, work
+function dropzone2(event) {
+  
+   event.preventDefault();
+  
+   if (event.type === "drop"){
+      let data = event.dataTransfer.getData("text");
+      event.target.appendChild(document.getElementById(data));
+   }
 
-
- bananabtn.addEventListener("click", function() {
-   if(image_placeholder2.hasChildNodes()){
-       banana2.attribute();
-     } else {
-       banana2.add();
-     }
- }, false); 
-
-
-
-
-
-function moveRight(){
-   imageplaceholder[0].setAttribute('style', "position: absolute"); // REQUIRED
-   imageplaceholder[0].style.right = 10 + "px";
-   console.log(imageplaceholder[0]);
 }
 
+monkey.addEventListener('dragstart', monkeyActions, false);
+sheep.addEventListener('dragstart', sheepActions, false);
+banana.addEventListener('dragstart', bananaActions, false);
 
-function moveLeft(){
-   imageplaceholder[0].setAttribute('style', "position: absolute"); // REQUIRED
-   imageplaceholder[0].style.left = 5 + "px";
-}
+dropzoneOne.addEventListener('dragover', allowDrop, false);
+dropzoneOne.addEventListener('drop', dropzone1, false);
 
-rightbtn.addEventListener("click", moveRight, false);
-   
-leftbtn.addEventListener("click", moveLeft, false);
+dropzoneTwo.addEventListener('dragover', allowDrop, false);
+dropzoneTwo.addEventListener('drop', dropzone2, false);
