9b4df1f052a2d43615aab3f5d62fc8bc94bf904a
Tania <tanialasell@gmail.com>
2020-12-30 10:17:52 -0500

    la la

--- dragdrop.js
+++ dragdrop.js
@@ -5,7 +5,6 @@ let sheep = document.getElementById('sheep');
 let banana = document.getElementById('banana');
 let dropzoneOne = document.getElementById('dropzone1');
 let dropzoneTwo = document.getElementById('dropzone2');
-let dropzoneThree = document.getElementById('dropzone3');
 
 
 ///////
@@ -13,42 +12,23 @@ let dropzoneThree = document.getElementById('dropzone3');
 ///////
 
 function monkeyActions(event) {
-   console.log(event.target.id);
+  console.log(event.target.id);
+  event.dataTransfer.setData("text", event.target.id);
   
-   if (event.type === "dragstart"){
-      event.dataTransfer.setData("text", event.target.id);
-
-   } else if(event.type === "mousedown"){
-     monkey.classList.add("imageSelected");
-   } 
-
 }
-
 
 
 function sheepActions(event) {
    console.log(event.target.id);
-  
-   if (event.type === "dragstart"){
-      event.dataTransfer.setData("text", event.target.id);
-
-   } else if(event.type === "mousedown"){
-      sheep.classList.add("imageSelected");
-   } 
-
+   event.dataTransfer.setData("text", event.target.id);
    
  }
 
 
-
  function bananaActions(event) {
-  console.log(event.target.id);
-  
-   if (event.type === "dragstart"){
-      event.dataTransfer.setData("text", event.target.id);
-
-   }
-
+   console.log(event.target.id);
+   event.dataTransfer.setData("text", event.target.id);
+   
  }
 
 
@@ -68,7 +48,6 @@ function dropzone1(event) {
    if (event.type === "drop"){
       let data = event.dataTransfer.getData("text");
       event.target.appendChild(document.getElementById(data));
-      dropzoneThree.innerText = "value of THIS: " + id;
    }
 
 }
@@ -86,13 +65,8 @@ function dropzone2(event) {
 }
 
 monkey.addEventListener('dragstart', monkeyActions, false);
-monkey.addEventListener('mousedown', monkeyActions, false);
-
 sheep.addEventListener('dragstart', sheepActions, false);
-sheep.addEventListener('mousedown', sheepActions, false);
-
 banana.addEventListener('dragstart', bananaActions, false);
-
 
 dropzoneOne.addEventListener('dragover', allowDrop, false);
 dropzoneOne.addEventListener('drop', dropzone1, false);
--- practicejs.js
+++ practicejs.js
@@ -5,6 +5,7 @@ let sheep = document.getElementById('sheep');
 let banana = document.getElementById('banana');
 let dropzoneOne = document.getElementById('dropzone1');
 let dropzoneTwo = document.getElementById('dropzone2');
+let dropzoneThree = document.getElementById('dropzone3');
 
 
 ///////
@@ -12,23 +13,42 @@ let dropzoneTwo = document.getElementById('dropzone2');
 ///////
 
 function monkeyActions(event) {
-  console.log(event.target.id);
-  event.dataTransfer.setData("text", event.target.id);
+   console.log(event.target.id);
   
+   if (event.type === "dragstart"){
+      event.dataTransfer.setData("text", event.target.id);
+
+   } else if(event.type === "mousedown"){
+     monkey.classList.add("imageSelected");
+   } 
+
 }
+
 
 
 function sheepActions(event) {
    console.log(event.target.id);
-   event.dataTransfer.setData("text", event.target.id);
+  
+   if (event.type === "dragstart"){
+      event.dataTransfer.setData("text", event.target.id);
+
+   } else if(event.type === "mousedown"){
+      sheep.classList.add("imageSelected");
+   } 
+
    
  }
 
 
+
  function bananaActions(event) {
-   console.log(event.target.id);
-   event.dataTransfer.setData("text", event.target.id);
-   
+  console.log(event.target.id);
+  
+   if (event.type === "dragstart"){
+      event.dataTransfer.setData("text", event.target.id);
+
+   }
+
  }
 
 
@@ -48,6 +68,7 @@ function dropzone1(event) {
    if (event.type === "drop"){
       let data = event.dataTransfer.getData("text");
       event.target.appendChild(document.getElementById(data));
+      dropzoneThree.innerText = "value of THIS: " + id;
    }
 
 }
@@ -65,8 +86,13 @@ function dropzone2(event) {
 }
 
 monkey.addEventListener('dragstart', monkeyActions, false);
+monkey.addEventListener('mousedown', monkeyActions, false);
+
 sheep.addEventListener('dragstart', sheepActions, false);
+sheep.addEventListener('mousedown', sheepActions, false);
+
 banana.addEventListener('dragstart', bananaActions, false);
+
 
 dropzoneOne.addEventListener('dragover', allowDrop, false);
 dropzoneOne.addEventListener('drop', dropzone1, false);
