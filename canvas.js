298a8c4da77bf7f68636a040736959e039530ab4
Tania <tanialasell@gmail.com>
2020-12-31 14:47:15 -0500

    color picker but needs CODE

--- canvas.js
+++ canvas.js
@@ -1,1 +1,7 @@
 var colorPicker = new iro.ColorPicker('#picker');
+
+var hex = colorPicker.color.hexString;
+console.log(hex); // hex = "#ff0000"
+
+headertitle = document.getElementsByTagName("h2");
+headertitle.style.color = hex;
