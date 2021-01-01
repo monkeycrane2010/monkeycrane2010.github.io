4c7e5a541de5e7f2b913e5d3c9913aeabab569ef
Tania <tanialasell@gmail.com>
2021-01-01 17:27:16 -0500

    revise

--- canvas.js
+++
@@ -1,7 +0,0 @@
-var colorPicker = new iro.ColorPicker('#picker');
-
-var hex = colorPicker.color.hexString;
-console.log(hex); // hex = "#ff0000"
-
-headertitle = document.getElementsByTagName("h2");
-headertitle.style.color = hex;
