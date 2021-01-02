2bdab09d114751bc17d6b98ae502f9ab5b9abb4f
Tania <tanialasell@gmail.com>
2021-01-02 09:36:17 -0500

    SUCCESS works -drag and drop-

--- bibleapp.js
+++ bibleapp.js
@@ -94,29 +94,50 @@ class Imagefactory {
       
 
       console.log(newimg);
-    //  newimg.addEventListener('mousedown', mouseDown, false);
-     
-      
 
+      /////
+      ///   Sample code from helpful website tutorial for drag n' drop
+      /////
 
-      newimg.addEventListener('dragstart', function(event){
-        event.dataTransfer.setData("text", event.target.id);
-        console.log(event.type);
+      newimg.addEventListener('mousedown', function(event){
+        // (1) prepare to moving: make absolute and on top by z-index
+        newimg.style.position = 'absolute';
+        newimg.style.zIndex = 1000;
+
+        // move it out of any current parents directly into body
+        // to make it positioned relative to the body
+        document.body.append(newimg);
+
+        // centers the ball at (pageX, pageY) coordinates
+        function moveAt(pageX, pageY) {
+          newimg.style.left = pageX - newimg.offsetWidth / 2 + 'px';
+          newimg.style.top = pageY - newimg.offsetHeight / 2 + 'px';
+        }
+
+        // move our absolutely positioned ball under the pointer
+        moveAt(event.pageX, event.pageY);
+
+        function onMouseMove(event) {
+          moveAt(event.pageX, event.pageY);
+        }
+
+        // (2) move the ball on mousemove
+        document.addEventListener('mousemove', onMouseMove);
+
+        // (3) drop the ball, remove unneeded handlers
+        newimg.onmouseup = function() {
+          document.removeEventListener('mousemove', onMouseMove);
+          newimg.onmouseup = null;
+        };
+       
       }, false);
 
 
-      dropzoneOne.addEventListener('dragover', function(event){
-        event.preventDefault();
-      }, false);
+      newimg.ondragstart = function() {
+        return false;
+      };
 
-
-      dropzoneOne.addEventListener('drop', function(event){
-        event.preventDefault();
-        let data = event.dataTransfer.getData("text");
-        event.target.appendChild(document.getElementById(data));
-      }, false);
-
-    }   
+  }
 
 }
 
