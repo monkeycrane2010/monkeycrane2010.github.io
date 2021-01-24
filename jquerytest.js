a3a321a0ec6ef4d87c24968ccc8fe20d633047d7
Tania <tanialasell@gmail.com>
2021-01-23 20:34:10 -0500

    l

--- jquerytest.js
+++ jquerytest.js
@@ -1,4 +1,26 @@
 $( function() {
     $( "#draggable" ).draggable();
-    $( "#draggable" ).resizable();
-  } );
+    $( "img" ).resizable();
+  });
+
+
+
+  $("button").on( "click", function() {
+    let counter;
+    counter = 2;
+    for(x=0; x <= counter; x++){
+       $("#one").append($("<img>").attr("src", "images/sheep.png").attr("id", x).draggable());
+    }
+
+  });
+
+  $("button").on( "click", function() {
+    let counter;
+    counter = 2;
+    for(x=0; x <= counter; x++){
+       $("#one").append($("<img>").attr("src", "images/sheep.png").attr("id", x).draggable());
+    }
+
+  });
+
+ 
