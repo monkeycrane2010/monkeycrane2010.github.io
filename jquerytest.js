70dca83bbc2abdca3b01c700fb8074aa94c2d883
Tania <tanialasell@gmail.com>
2021-01-24 22:05:47 -0500

    sheep

--- jquerytest.js
+++ jquerytest.js
@@ -1,72 +1,39 @@
-$( function() {
+$( document ).ready(function() {
     $("#draggable").draggable();
     $("#draggable").resizable();
 
+  
 
     $("#sheepbtn").on( "click", function() {
-   
-      $("#one").append($("<img>").attr("src", "images/sheep.png").draggable());   // CREATE NEW
-  
-        
-      $( "img" ).each(function( index ) {    // EACH
+      $("#one").append($("<img>").attr("src", "images/sheep.png").draggable());   // CREATE NEW 
+      $( "img" ).each(function( index ) {     // EACH
         $(this).attr("id", "sheep"  + index);
         console.log( index + ": " + $( this ).text() );
       });
-  
-     
-  
-      
-    $("img").on("click", function(event) {   // CLASS
-      $('h1').html(this.id);
-      $(this).toggleClass("selected");
-  
+      $("img").on("mousemove", function() {    // TOGGLE CLASS
+        $('h1').html(this.id);
+        $(this).addClass('dot');
+      });
+      $("img").on("mouseleave", function() {    // TOGGLE CLASS
+        $(this).removeClass('dot');
+      });
     });
-  
-
-  });
-  
-  
-
-
-  $("#newtextbtn").on( "click", function() {
-   
-    $("#one").append($("<p>Edit this</p>").css("color", "green").css("font-size", "20px").attr("contentEditable", true).draggable());   // CREATE TEXT
-
-});
-
-
 
   
-   
-  $("#deletebtn").on( "click", function() {
-  
-    $('h1').html("you just clicked" + ", " + this.id);
-    $('#one').empty();
-  
-  });
-    
-   
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-});
-
-
 
  
+    
+ 
+   
+
+});
+
+
+
+
+
+
+
+
+
+
