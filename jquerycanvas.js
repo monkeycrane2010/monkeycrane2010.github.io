792addcd512608a9bc96a961205836def594e16c
Tania <tanialasell@gmail.com>
2021-01-31 12:43:59 -0500

    blah blah blah

--- jquerycanvas.js
+++ jquerycanvas.js
@@ -1,8 +1,31 @@
 $( document ).ready(function() {
  
-    $( ".scene" ).click(function() {
-        $(".stamp-parent1").toggle();
+  $( ".scene" ).click(function() {
+    
+    $(".stamp-parent1").toggle();
+    $(".stamp-parent1 > li").each(function() {
+      $(this).draggable();
+    });
 
-      });
+  });
+
+
+  $( ".animal" ).click(function() {
+    $(".stamp-parent2").toggle();
+    $(".stamp-parent2 > li").each(function() {
+      $(this).draggable();
+    });
+
+  });
+
+  $( ".people" ).click(function() {
+    $(".stamp-parent3").toggle();
+    $(".stamp-parent3 > li").each(function() {
+      $(this).draggable();
+    });
+
+  });
+
+
 
 });
