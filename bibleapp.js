60396c7d8e9b81dbd72dc15919ee6a4fc4490db4
Tania <tanialasell@gmail.com>
2021-01-31 16:49:50 -0500

    clone

--- bibleapp.js
+++ bibleapp.js
@@ -1,34 +1,22 @@
 $( document ).ready(function() {
  
   $( ".stamp" ).click(function() {
-    
-    $(".stamp-parent1").toggle();
-    $(".stamp-parent1 > li").each(function() {
-      $(this).draggable();
-    });
-
-
-    $(".stamp-parent2").toggle();
-    $(".stamp-parent2 > li").each(function() {
-      $(this).draggable();
-    });
-
-
-    $(".stamp-parent3").toggle();
-    $(".stamp-parent3 > li").each(function() {
-      $(this).draggable();
-    });
-
+    $(".stamp-main").toggle();
   });
 
-  $( ".people" ).click(function() {
-    $(".stamp-parent3").toggle();
-    $(".stamp-parent3 > li").each(function() {
-      $(this).click(function() {
-        $(this).clone().appendTo(".myworkspace").draggable();
-      });
-    });
+  $(".stamp-parent1 > li").click(function() {
+    $(this).clone().appendTo(".myworkspace").draggable();
   });
+
+  $(".stamp-parent2 > li").click(function() {
+    $(this).clone().appendTo(".myworkspace").draggable();
+  });
+
+  $(".stamp-parent3 > li").click(function() {
+    $(this).clone().appendTo(".myworkspace").draggable();
+  });
+  
+
 
 
   $( ".mycaption" ).attr("contenteditable", true);
