61e7aa538e793849e4524200197b0313df8ed385
Tania <tanialasell@gmail.com>
2021-01-31 20:37:40 -0500

    delete

--- bibleapp.js
+++ bibleapp.js
@@ -6,26 +6,48 @@ $( document ).ready(function() {
   });
 
   $(".stamp-parent1 > li").click(function() {
+    $(this).addClass("sceneclone");
     $(this).clone().appendTo(".cloneditems").draggable();
+    $(".sceneclone").click(function(){
+      $(this).toggleClass("selected");
+    });
   });
 
   $(".stamp-parent2 > li").click(function() {
+    $(this).addClass("sceneclone");
     $(this).clone().appendTo(".cloneditems").draggable();
+    $(".sceneclone").click(function(){
+      $(this).toggleClass("selected");
+    });
   });
 
   $(".stamp-parent3 > li").click(function() {
+    $(this).addClass("sceneclone");
     $(this).clone().appendTo(".cloneditems").draggable();
+    $(".sceneclone").click(function(){
+      $(this).toggleClass("selected");
+    });
   });
-  
 
+ 
+  $(".delete").click("mouseup", function(){    
+    $(".selected").remove();
+  });
+
+
+  $( ".print" ).click(function() {
+    window.print();
+   });
+  
   
   $( ".text" ).click(function() {
     $( ".cloneditems" ).append( "<p>Click to EDIT</p>" );
     $(".cloneditems > p").draggable();
     $(".cloneditems > p").attr("contenteditable", true);
   });
- 
- 
 
 
+
+
+ 
 });
