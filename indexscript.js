c3c3d1141abaf4ab614ace5b7f9e770e3639a770
Tania <tanialasell@gmail.com>
2021-02-08 13:35:14 -0500

    monk whiteboard

---
+++ indexscript.js
@@ -0,0 +1,66 @@
+$( document ).ready(function() {
+ 
+
+    $( ".stamp" ).click(function() {
+      $(".stamp-main").toggle();
+    });
+  
+  
+    $(".stamp-parent1 > li").click(function() {
+      $(this).addClass("sceneclone");
+      $(this).clone().appendTo(".cloneditems").draggable();
+      $(".sceneclone").click(function(){
+        $(this).toggleClass("selected");
+      });
+    });
+  
+    $(".stamp-parent2 > li").click(function() {
+      $(this).addClass("sceneclone");
+      $(this).clone().appendTo(".cloneditems").draggable();
+      $(".sceneclone").click(function(){
+        $(this).toggleClass("selected");
+      });
+    });
+  
+    $(".stamp-parent3 > li").click(function() {
+      $(this).addClass("sceneclone").clone().appendTo(".cloneditems").draggable();
+      $(".sceneclone").click(function(){
+        $(this).toggleClass("selected");
+      });
+    });
+  
+   
+    $(".delete").click("mouseup", function(){    
+      $(".selected").remove();
+    });
+  
+  
+    $( ".print" ).click(function() {
+      window.print();
+     });
+    
+    
+    $( ".text" ).click(function() {
+      $( ".cloneditems" ).append( "<p>Click to EDIT</p>" );
+      $(".cloneditems > p").draggable();
+      $(".cloneditems > p").attr("contenteditable", true);
+    });
+  
+  
+    $( ".addslide" ).click(function() {
+     alert("yay you clicked me");
+     $( ".replaceme" ).replaceWith( $( "h1" ) );
+     $(".singleslide").clone().insertAfter(this);
+    });
+    
+  
+    $('body').keypress("keydown", function(event){
+        event.preventDefault();
+        if(event.key === "a"){
+          alert(event.key);
+          $("#box").style.bottom += "8px";
+        }
+  
+    });
+   
+  });
