949ce25255c267f9cbddb2cc6346d795aee7a2d3
Tania <tanialasell@gmail.com>
2021-05-29 12:32:22 -0400

    erase

--- wb techteacher/script.js
+++ wb techteacher/script.js
@@ -1,8 +1,9 @@
 console.log ('X plus 2');
 
-const cb = document.querySelector('#accept');
-const btn = document.querySelector('#btn');
-btn.onclick = () => {
-    const result = cb.value;
-    alert(result); // on
-};
+$( document ).ready(function() {
+
+
+    
+
+  
+});
