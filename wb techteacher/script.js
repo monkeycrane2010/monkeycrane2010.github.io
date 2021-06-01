149f90d3461d143cb12d523fd4810607384a7821
Tania <tanialasell@gmail.com>
2021-06-01 12:46:12 -0400

    parse

--- wb techteacher/script.js
+++ wb techteacher/script.js
@@ -1,6 +1,15 @@
 $( document ).ready(function() {
     // Handler for .ready() called.
 
+// Clicks on gray buttons
+$('input.btn-secondary').on("click", function(){
+    let test = $(this).prev().css({"border": "2px solid green"});
+    let datafield = $(this).prev().data('topic');
+    console.log('you clicked ' + test );
+    console.log('DATA: ' + datafield );
+    datafield.append()
+
+});
 
 
 
