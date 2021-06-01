4429b8aeb81e7699a57808254cfc3a948d354618
Tania <tanialasell@gmail.com>
2021-06-01 15:32:51 -0400

    fracture

--- wb techteacher/script.js
+++ wb techteacher/script.js
@@ -1,6 +1,12 @@
 $( document ).ready(function() {
     // Handler for .ready() called.
 
+// Search 
+$('#basic-addon1').on('click', function(){
+    let str = $('#searchbox').val();
+    alert(str);
+});
+
 // Clicks on gray buttons
 $('input.btn-secondary').on("click", function(){
     let test = $(this).prev().css({"border": "2px solid green"});
@@ -10,6 +16,7 @@ $('input.btn-secondary').on("click", function(){
     datafield.append()
 
 });
+
 
 
 
