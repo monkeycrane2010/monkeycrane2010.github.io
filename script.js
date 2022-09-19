a12a28bbbcba033230bf1053fa4a312df10233d4
monkeycrane2010 <38597281+monkeycrane2010@users.noreply.github.com>
2022-09-18 20:29:55 -0400

    Back to square 1

--- script.js
+++ script.js
@@ -5,55 +5,7 @@ $( document ).ready(function() {
     });
   
   
-    $(".stamp-parent1 > li").click(function() {    // SCENE images
-      $(this).addClass("sceneclone").clone().appendTo(".cloneditems").draggable();
-      $(".sceneclone").click(function(){
-        $(this).toggleClass("selected");
-      });
-      
-    });
-  
-    $(".stamp-parent2 > li").click(function() {
-      $(this).addClass("sceneclone").clone().appendTo(".cloneditems").draggable();
-      $(".sceneclone").click(function(){
-        $(this).toggleClass("selected");
-      });
-    });
-  
-    $(".stamp-parent3 > li").click(function() {
-      $(this).addClass("sceneclone").clone().appendTo(".cloneditems").draggable();
-      $(".sceneclone").click(function(){
-        $(this).toggleClass("selected");
-      });
-    });
-  
-   
-    $(".delete").click("mouseup", function(){    
-      $(".selected").remove();
-    });
-  
-  
-    $( ".print" ).click(function() {
-      window.print();
-     });
-    
-    
-    $( ".text" ).click(function() {
-      $( ".cloneditems" ).append( "<p>Click to EDIT</p>" );
-      $(".cloneditems > p").draggable();
-      $(".cloneditems > p").attr("contenteditable", true);
-    });
-  
-  
-    $( ".addslide" ).click(function() {
-     alert("+New Slide created");
-     $(".addslide").after('<div class="singleslide"></div>');
-    });
-
-    $('.singleslide').each(function(index){
-      alert("hello " + index);
-
-    });
+ 
 
   
    
